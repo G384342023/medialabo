592e40acbee0f858e4cd530f9baa6dbb2a27b2bb
G384342023 <238434@st.takushoku-u.ac.jp>
2024-07-01 00:03:09 +0900

    課題4-1

--- kazuate.js
+++ kazuate.js
@@ -1,51 +1,43 @@
 // 答え
-let kotae = Math.floor(Math.random() * 10) + 1;
+let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-let kiroku = 0;
 
-let kakak = document.querySelector('span#kaisu');
-
-let p = document.createElement('p');
-
-p.
-
-hantei();
-hantei();
-hantei();
+// 予想を1回実行する
 hantei();
 
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso = 4;
+  
+  kaisu++;  // 回答回数を1増やす
+  
+  // 回数と予想を表示
+  document.getElementById('kaisu').textContent = kaisu;
+  document.getElementById('answer').textContent = yoso;
 
-    let yoso = 4;
+  // 結果を表示するための変数
+  let result = "";
 
-    // 課題3-1: 正解判定する
-    // kotae と yoso が一致するかどうか調べて結果を出力
-    // 課題3-1における出力先はコンソール
-    kaisu += 1;
-    kiroku += 1;
-
-    if (kiroku < 4) {
-        if (kotae === yoso) {
-            console.log("正解です。おめでとう！");
-            kiroku = 4;
-        } else if (kotae < yoso) {
-            console.log("まちがい。答えはもっと小さいですよ");
-        } else {
-            console.log("まちがい。答えはもっと大きいですよ");
-        }
-    } else if (kiroku === 4) {
-        if (kotae === yoso) {
-            console.log("正解です。おめでとう！");
-        } else {
-            console.log("まちがい。残念でした答えは" + kotae + "です。");
-        }
+  if (kaisu > 3) {
+    result = "答えは " + kotae + " でした．すでにゲームは終わっています";
+  } else if (yoso === kotae) {
+    result = "正解です．おめでとう!";
+  } else {
+    if (kaisu === 3) {
+      result = "まちがい．残念でした答えは " + kotae + " です．";
     } else {
-        console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
+      if (yoso < kotae) {
+        result = "まちがい．答えはもっと大きいですよ";
+      } else {
+        result = "まちがい．答えはもっと小さいですよ";
+      }
     }
+  }
+
+  // 結果を表示
+  document.getElementById('result').textContent = result;
 }
